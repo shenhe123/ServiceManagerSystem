a80c1668bcebdaa3423556b11df0e8b20d072925
he.shen <1527360050@qq.com>
2023-09-07 20:58:53 +0800

    工单详情页 去排查按钮增加 已排查完状态

--- app/src/main/java/com/jssg/servicemanagersystem/ui/workorder/fragment/WorkOrderDetailFragment.kt
+++ app/src/main/java/com/jssg/servicemanagersystem/ui/workorder/fragment/WorkOrderDetailFragment.kt
@@ -11,7 +11,9 @@ import com.jssg.servicemanagersystem.ui.account.entity.MenuEnum
 import com.jssg.servicemanagersystem.ui.workorder.AddWorkOrderCheckActivity
 import com.jssg.servicemanagersystem.ui.workorder.WorkOrderDetailActivity
 import com.jssg.servicemanagersystem.ui.workorder.entity.WorkOrderInfo
+import com.jssg.servicemanagersystem.utils.BigDecimalUtils.bigDecimal
 import com.jssg.servicemanagersystem.utils.RolePermissionUtils
+import com.jssg.servicemanagersystem.utils.toast.ToastUtils
 
 class WorkOrderDetailFragment : Fragment() {
     private lateinit var binding: FragmentWorkOrderDetailBinding
@@ -52,17 +54,17 @@ class WorkOrderDetailFragment : Fragment() {
 
             binding.tvProductDesc.text = "问题描述：${it.productDes}"
             binding.tvProductCode.text = "产品编号：${it.productCode}"
-            binding.tvProductNum2.text = "问题数量：${it.productNum ?: "0"}"
+            binding.tvProductNum.text = "问题数量：${it.productNum ?: "0"}"
 
             binding.tvClientName.text = "服务客户名称：${it.customer ?: ""}"
             binding.tvServiceName.text = "服务人员名称：${it.salesManager}"
             binding.tvTel.text = "联系方式：${it.tel}"
-            binding.tvCheckNum.text = "排查数量：${it.checkNum ?: 0}"
+            binding.tvCheckNum.text = "总排查数量：${it.checkNum ?: 0}"
             binding.tvUnitPrice.text = "服务单价：${it.unitPrice}"
             binding.tvServicePeriod.text = "预估服务周期：${it.servicePeriod}"
             binding.tvTotalPrice.text = "预估总费用：${it.totalPrice}"
             binding.tvAddress.text = "服务地点：${it.serviceAdd}"
-            binding.tvProductNum.text = "不良数量：${it.productNum ?: "0"}"
+            binding.tvCheckNumTotal.text = "已排查数量：${it.checkNumTotal ?: "0"}"
             binding.etRemark.setText(it.remark)
 
             //已排查的隐藏去排查按钮
@@ -70,8 +72,18 @@ class WorkOrderDetailFragment : Fragment() {
             // 1 -> = "排查中"
             // 2 -> = "已完成"
 
-            binding.mbtCheckOrder.isVisible =
+            val isVisible =
                 it.checkState != 2 && RolePermissionUtils.hasPermission(MenuEnum.QM_WORKORDERDETAIL_ADD.printableName)
+            binding.mbtCheckOrder.isVisible = isVisible
+
+            if (isVisible) {
+                //已排查的总数 小于 总共需要排查的数量
+                val canCheck = it.checkNumTotal.bigDecimal() < it.checkNum.bigDecimal()
+                binding.mbtCheckOrder.isEnabled = canCheck
+                if (!canCheck) {
+                    binding.mbtCheckOrder.text = "已排查完"
+                }
+            }
         }
 
         binding.mbtCheckOrder.setOnClickListener {
