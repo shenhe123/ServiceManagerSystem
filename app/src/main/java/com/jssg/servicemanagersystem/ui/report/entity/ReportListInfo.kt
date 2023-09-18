bac1d3737b204191d7e092e02004af4fd7732cd3
he.shen <1527360050@qq.com>
2023-09-18 22:28:12 +0800

    报表 字段 修改

--- app/src/main/java/com/jssg/servicemanagersystem/ui/report/entity/ReportListInfo.kt
+++ app/src/main/java/com/jssg/servicemanagersystem/ui/report/entity/ReportListInfo.kt
@@ -12,40 +12,51 @@ import kotlinx.android.parcel.Parcelize
 @SmartTable(name = "报表预览")
 @Parcelize
 data class ReportListInfo(
-    @SmartColumn(id = 1,name ="申请人")
+    @SmartColumn(id = 1,name ="工单号")
+    val billNo: String?,
+    @SmartColumn(id = 2,name ="申请人")
     val applyName: String,
-    @SmartColumn(id = 2,name ="申请日期")
+    @SmartColumn(id = 3,name ="申请日期")
     val applyDate: String,
-    @SmartColumn(id = 3,name ="申请部门")
+    @SmartColumn(id = 4,name ="申请部门")
     val applyDept: String,
-    @SmartColumn(id = 4,name ="申请工厂")
+    @SmartColumn(id = 5,name ="申请工厂")
     val orgService: String,
-    @SmartColumn(id = 5,name ="服务内容描述")
+    @SmartColumn(id = 6,name ="产品名称")
     val productDes: String,
-    @SmartColumn(id = 6,name ="服务人员名称")
+    @SmartColumn(id = 7,name ="产品编码")
+    val productCode: String,
+    @SmartColumn(id = 8,name ="内容描述")
+    val remark: String,
+    @SmartColumn(id = 9,name ="服务人员名称")
     val checkerName: String?,
-    @SmartColumn(id = 7,name ="排查数量")
+    @SmartColumn(id = 10,name ="排查数量")
     val checkNum: Int,
-    @SmartColumn(id = 8,name ="服务单价")
+    @SmartColumn(id = 11,name ="服务单价")
     val unitPrice: Int,
-    @SmartColumn(id = 9,name ="预估总费用")
+    @SmartColumn(id = 12,name ="预估总费用")
     val totalPrice: Int,
-    @SmartColumn(id = 10,name ="服务周期")
+    @SmartColumn(id = 13,name ="服务周期")
     val servicePeriod: String,
-    @SmartColumn(id = 11,name ="服务地点")
+    @SmartColumn(id = 14,name ="服务地点")
     val serviceAdd: String?,
-    @SmartColumn(id = 12,name ="排查开始日期")
+    @SmartColumn(id = 15,name ="排查开始日期")
     val checkBegin: String?,
-    @SmartColumn(id = 13,name ="排查结束日期")
+    @SmartColumn(id = 16,name ="排查结束日期")
     val checkEnd: String?,
-    @SmartColumn(id = 14,name ="工厂CQE工程师")
+    @SmartColumn(id = 17,name ="工厂CQE工程师")
     val approverName: String?,
-    @SmartColumn(id = 15,name ="排查数量总（至今）")
+    @SmartColumn(id = 18,name ="审核意见")
+    val applyRemark: String?,
+    @SmartColumn(id = 19,name ="排查数量总（至今）")
     val checkNumTotal: Int,
-    @SmartColumn(id = 16,name ="不良数量总（至今）")
+    @SmartColumn(id = 20,name ="不良数量总（至今）")
     val badNumTotal: Int,
-    @SmartColumn(id = 17,name ="工单状态")
+    @SmartColumn(id = 21,name ="工单状态")
     var checkStateName: String?,
+
+
+
     val badNum: Int,
     val badPicNames: String,
     val batchPicNames: String,
